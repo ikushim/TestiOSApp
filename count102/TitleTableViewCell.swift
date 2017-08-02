618e6d143a95072395e311b02cb7a5a8a9270b5a
nttr <nttr@BI-23561.local>
2017-08-02 15:09:05 +0900

    First Commit.

---
+++ count102/TitleTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TitleTableViewCell.swift
+//  count102
+//
+//  Created by nttr on 2017/08/02.
+//  Copyright © 2017年 nttr. All rights reserved.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    @IBOutlet var titleLabel : UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- count102/ViewController.swift
+++ count102/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number : Int = 0
+    @IBOutlet var label : UILabel!
+    @IBOutlet var titleTable : UITableView!
+//    var titleArray = {"りんご";"みかん";"スイカ"}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        /*
+        titleTable.dataSource = self
+        titleTable.delegate = self
+        let nib = UINib(nibName: nib, bundle: Bundle.main)
+        */
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +32,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
 
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+    }
 
+    /*
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+ */
 }
 
